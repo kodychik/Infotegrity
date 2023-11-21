6126dfac087f316bce67632d4f4a85695eb48c75
kodychik <73432901+kodychik@users.noreply.github.com>
2023-11-21 10:15:12 -0500

    sentiment

---
+++ sentiment.py
@@ -0,0 +1,8 @@
+# Importing the essential libraries
+# Beautiful Soup is a Python library for pulling data out of HTML and XML files
+# The Natural Language Toolkit
+import torch
+
+
+x = torch.rand(5, 3)
+print(x)
